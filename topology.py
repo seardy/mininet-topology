129ad4c55f8622148aeb9aa018d55b998d6300e0
seardy <sarteaga@protonmail.com>
2018-07-05 00:24:18 -0500

    revert

--- topology.py
+++ topology.py
@@ -10,13 +10,13 @@ def topology():
 	net = Mininet_wifi (controller=Controller, accessPoint=UserAP, disableAutoAssociation=True)
 
 	# Access Point sala de sistemas
-	ap1 = net.addAccessPoint('sala_sist',ssid="Estudiantes,Docentes", mode='g', channel='11', mac='e0:10:7f:af:b7:a8', range=20, position="15,30,0")
+	ap1 = net.addAccessPoint('ap1',ssid="Estudiantes,Docentes", mode='g', channel='11', mac='e0:10:7f:af:b7:a8', range=20, position="15,30,0")
 	# Access Point Segundo piso de biblioteca
-	ap2 = net.addAccessPoint('segundo_piso',ssid="Estudiantes,Docentes", mode='g', channel='6', mac='f0:b0:52:7a:31:19', range=20,position="30,40,0")
+	ap2 = net.addAccessPoint('ap2',ssid="Estudiantes,Docentes", mode='g', channel='6', mac='f0:b0:52:7a:31:19', range=20,position="30,40,0")
 	# Access Point restante (no encontrado)
 	ap3 = net.addAccessPoint('ap3',ssid="Estudiantes,Docentes", mode='g', channel='1', mac='2c:5d:93:bd:83:5c',range=20,position="50,50,0")
 
-	sta1 = net.addStation('Sta1', mac="00:00:00:00:00:02", ip="10.0.0.2/8", position="10,20,0", range=20)
+	sta1 = net.addStation('sta1', mac="00:00:00:00:00:02", ip="10.0.0.2/8", position="10,20,0", range=20)
 
 
 	#Controlador
