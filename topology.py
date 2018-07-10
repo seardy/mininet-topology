f73f93fdaf98580a03c76be40e15bb93751f5773
seardy <sarteaga@protonmail.com>
2018-07-10 15:25:03 -0500

    Study case added

--- topology.py
+++ topology.py
@@ -7,37 +7,59 @@ from mininet.wifi.node import UserAP
 from mininet.wifi.cli import CLI_wifi
 
 def topology():
-	net = Mininet_wifi (controller=Controller, accessPoint=UserAP, disableAutoAssociation=True)
+	
+	# Creating network
+	net = Mininet_wifi (
+			controller=Controller, 
+			accessPoint=UserAP, 
+			disableAutoAssociation=True)
 
-	# Access Point sala de sistemas
-	ap1 = net.addAccessPoint('ap1',ssid="Estudiantes,Docentes,Administrativo", mode='g', channel='11', mac='e0:10:7f:af:b7:a8', range=20, position="15,30,0")
-	# Access Point Segundo piso de biblioteca
-	ap2 = net.addAccessPoint('ap2',ssid="Estudiantes,Docentes,Administrativo", mode='g', channel='6', mac='f0:b0:52:7a:31:19', range=20,position="30,40,0")
-	# Access Point restante (no encontrado)
-	ap3 = net.addAccessPoint('ap3',ssid="Estudiantes,Docentes,Administrativo", mode='g', channel='1', mac='2c:5d:93:bd:83:5c',range=20,position="50,50,0")
+	# Access Point -> Facultad de la salud (Acreditación)
+	ap1 = net.addAccessPoint('ap1',
+		ssid="Estudiantes,Docentes,Administrativo",
+		mode='g', channel='11', mac='e0:10:7f:af:b7:a8', range=20, position="40.8854,29.1667,0")
+	
+	# Access Point -> Facultad de la salud (Sala de cómputo)
+	ap2 = net.addAccessPoint('ap2',
+		ssid="Estudiantes,Docentes,Administrativo",
+		mode='g', channel='6', mac='f0:b0:52:7a:31:19', range=20, position="68.4896,52.3437,0")
+	
+	# Access Point -> Facultad de la salud (Maestria_temp)
+	ap3 = net.addAccessPoint('ap3',
+		ssid="Estudiantes,Docentes,Administrativo",
+		 mode='g', channel='1', mac='2c:5d:93:bd:83:5c',range=20,position="47.9167,66.6667,0")
 
-	sta1 = net.addStation('sta1', mac="00:00:00:00:00:02", ip="10.0.0.2/8", position="10,20,0", range=20)
+
+	# Station -> Station 1
+
+	sta1 = net.addStation('sta1', mac="00:00:00:00:00:02",
+		 ip="10.0.0.2/8", position="10,20,0", range=20)
 
 
-	#Controlador
+	# Adding Controller to Network
 	c1 = net.addController('c1', controller=Controller)
 
+	# Establishing Propagation Model for network
 	net.propagationModel(model="logDistancePropagationLossModel", exp=3.5)
+
 
 	info("*** Configuring wifi nodes \n")
 	net.configureWifiNodes()
 
-	#Enlaces
+	# Adding links between APs
 	net.addLink(ap1,ap2, link="wired")
 	net.addLink(ap2,ap3, link="wired")
-	#net.addLink(sta1, ap1)
-
 	
-	net.plotGraph(max_x=1400, max_y=1000)
+	net.plotGraph(max_x=100, max_y=100)
 
 	info(" *** Starting network *** \n")
+	# Initialize network
 	net.build()
+
+	# Starting up Controller
 	c1.start()
+
+	# Starting up APs
 	ap1.start([c1])
 	ap2.start([c1])
 	ap3.start([c1])
